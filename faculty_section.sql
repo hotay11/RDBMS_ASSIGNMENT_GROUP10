085fee40d9ae47f59181bacca643d122af8c454f
hotay11 <j.hotay@alustudent.com>
2026-07-23 16:44:16 +0300

    Update faculty_section.sql

--- faculty_section.sql
+++ faculty_section.sql
@@ -5,23 +5,28 @@ CREATE DATABASE IF NOT EXISTS alu_db;
 
 USE alu_db;
 
--- Create table
 CREATE TABLE Students (
     student_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     date_of_birth DATE,
     email VARCHAR(100) UNIQUE,
-    enrollment_date DATE
+    enrollment_date DATE,
+    classroom_id INT              
+);
+...
+ALTER TABLE Students              
+    ADD CONSTRAINT fk_students_classroom
+    FOREIGN KEY (classroom_id) REFERENCES Classroom(classroom_id);
 );
 
 -- Insert 5 sample rows
-INSERT INTO Students (first_name, last_name, date_of_birth, email, enrollment_date) VALUES
-('Aisha', 'Karenzi', '2008-03-15', 'aisha.k@school.edu', '2023-09-01'),
-('Brian', 'Mugisha', '2007-11-02', 'brian.m@school.edu', '2023-09-01'),
-('Cynthia', 'Iradukunda', '2008-06-20', 'cynthia.i@school.edu', '2023-09-01'),
-('David', 'Niyonsenga', '2007-01-10', 'david.n@school.edu', '2023-09-01'),
-('Esther', 'Uwimana', '2008-09-05', 'esther.u@school.edu', '2023-09-01');
+INSERT INTO Students (first_name, last_name, date_of_birth, email, enrollment_date, classroom_id) VALUES
+('Aisha', 'Karenzi', '2008-03-15', 'aisha.k@school.edu', '2023-09-01', 1),
+('Brian', 'Mugisha', '2007-11-02', 'brian.m@school.edu', '2023-09-01', 2),
+('Cynthia', 'Iradukunda', '2008-06-20', 'cynthia.i@school.edu', '2023-09-01', 3),
+('David', 'Niyonsenga', '2007-01-10', 'david.n@school.edu', '2023-09-01', 1),
+('Esther', 'Uwimana', '2008-09-05', 'esther.u@school.edu', '2023-09-01', 4);
 
 -- UPDATE: change a student's email
 UPDATE Students SET email = 'aisha.karenzi@school.edu' WHERE student_id = 1;
